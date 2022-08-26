a493501c6567ecd292916cd3a9f3dca3fefd87ba
raghav-sethi <76265403+raghav-sethi@users.noreply.github.com>
2022-08-27 03:39:38 +0530

    Card component added

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "./../UI/Card";
+
+export default function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          date={expense.date}
+          amount={expense.amount}
+        />
+      ))}
+    </Card>
+  );
+}
+
+// <ExpenseItem
+//   title={props.expenses[0].title}
+//   date={props.expenses[0].date}
+//   amount={props.expenses[0].amount}
+// />
+// <ExpenseItem
+//   title={props.expenses[1].title}
+//   date={props.expenses[1].date}
+//   amount={props.expenses[1].amount}
+// />
+// <ExpenseItem
+//   title={props.expenses[2].title}
+//   date={props.expenses[2].date}
+//   amount={props.expenses[2].amount}
+// />
+// <ExpenseItem
+//   title={props.expenses[3].title}
+//   date={props.expenses[3].date}
+//   amount={props.expenses[3].amount}
+// />
